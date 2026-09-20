ba532d771c8bab97d1f9553c1b986db22b615579
qhy991 <846726600@qq.com>
2026-09-20 17:09:56 +0800

    Read MetaX instruction admission from its Target alongside other platforms

--- src/open_cake_ir/tasks/devices.py
+++ src/open_cake_ir/tasks/devices.py
@@ -97,7 +97,7 @@ BACKENDS = {
     "triton-metax": {"target": "xcore1002", "device_name": "MetaX C550",
                      "provenance_token": "C550", "route": "triton",
                      "allocation": "local_broker",
-                     "tanh_contract": None, "timing_source": None,
+                     "timing_source": None,
                      "power_of_two_width": True},
 }
 
